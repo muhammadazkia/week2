c3636666e87e37282038efb8d394bea86653a881
muhammad azkia <sir.azkia@gmail.com>
2017-12-22 17:08:18 +0700

    tugas satu

---
+++ exercises-1.js
@@ -0,0 +1,40 @@
+//bagian variabel number
+var number1=33;
+console.log(number1);
+
+var number2=6;
+console.log(number2);
+
+//bagian variabel string
+var string1='foo';
+console.log(string1);
+
+var string2='bar';
+console.log(string2);
+
+//bagian variabel boolean
+var boolean1=true;
+console.log(boolean1);
+
+var boolean2=false;
+console.log(boolean2);
+
+//bagian ini hanya sekedar eksperimen saja
+//bukan bagian dari exercise
+var numberSatu=33, numberDua=6;
+console.log(numberSatu+numberDua);
+
+var numberSatu=49, numberDua=3;
+console.log(numberSatu*numberDua);
+
+var numberSatu=30, numberDua=6, numberTiga=3;
+console.log(numberSatu/numberDua);
+console.log (numberDua+numberTiga*numberSatu);
+
+
+
+
+
+
+
+
